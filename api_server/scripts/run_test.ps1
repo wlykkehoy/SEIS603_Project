65444a055c069cd7592fdb194600fe2d43986dec
Wade Lykkehoy <WadeLykkehoy@gmail.com>
2020-07-02 18:09:55 -0500

    some cleanup

--- api_server/scripts/run_test.ps1
+++ api_server/scripts/run_test.ps1
@@ -1,6 +1,6 @@
 #clear; python .\test_main.py -v -t test_1_message_in_range
 #clear; python .\test_main.py -v -t test_10_messages_in_range
-clear; python .\test_main.py -v -t test_trigger_temp_alert
-#clear; python .\test_main.py -v -t test_trigger_and_clear_temp_alert
+#clear; python .\test_main.py -v -t test_trigger_temp_alert
+clear; python .\test_main.py -v -t test_trigger_and_clear_temp_alert
 
 #clear; pytest test_main.py
--- api_server/test_main.py
+++ api_server/test_main.py
@@ -26,59 +26,61 @@ CONFIG_DATA = {'get_count_url': {'readings':      'http://' + IP_ADDR + '/readin
 verbose = False
 
 
-def delete_records(collections_to_clear, dev_id=None):
+def delete_resources(resource_names, dev_id=None):
     """
-    Utility function to delete all records in the passed list of tables/collections. We do this by sending
-    a DELETE request for each specified table/collection. If dev-id is specified, only deletes for the
-    specified device, else deletes for all devices.
+    Utility function to delete instances of the specified resource(s) (i.e. docs from the specified
+    collections in our MongoDB). We do this by sending a DELETE reqeust for each
+    specified resource. If a device ID is specified, only instances of the resouce(s) for that device ID
+    are deleted. If a device ID is not specified, all instances of the resource(s) are deleted.
 
     Args:
-        collections_to_clear:   List of collections (resources) to send
-        dev_id (str):           ID of device to delete records for; if not specified records are
-                                  deleted for all devices.
+        resource_names:   List of the names of the resources to delete
+        dev_id (str):     ID of device to delete resource instances for; if not specified instances are
+                            deleted for all devices.
 
     Returns:
         Status code from the HTTP request.
     """
     if verbose:
-        print('Clearing collections {}, dev_id=\'{}\''.format(collections_to_clear, dev_id), flush=True)
+        print('Deleting {}, dev_id=\'{}\''.format(resource_names, dev_id), flush=True)
 
-    assert len(collections_to_clear) > 0        # this should never happen...
+    assert len(resource_names) > 0        # this should never happen...
 
     params = {}
     if dev_id is not None:
         params = {'dev-id': dev_id}
     response = None     # here just to get rid of PEP8 warning
-    for collection in collections_to_clear:
-        response = requests.delete(CONFIG_DATA['delete_url'][collection], params=params)
+    for resource in resource_names:
+        response = requests.delete(CONFIG_DATA['delete_url'][resource], params=params)
         if response.status_code != 200:  # stop on the first failing HTTP request
             break
 
     return response.status_code
 
 
-def doc_count(collection, dev_id, alert_type=None):
+def resource_count(resource_name, dev_id, reading_type=None):
     """
-    Utility function to count records in the passed tables/collections for the passed device ID.
-    If the alert type is passed, only a count of those specific alert types are returned, else
-    a count of any/all alert types is returned.
+    Utility function to count the number of a specified resource for the specified device ID (i.e.
+    the number of docs in our MongoDB). If the reading type is passed, only a count of those
+    specific reading types are returned, else a count of any/all reading types is returned.
 
     Args:
-        dev_id (str):       ID of device to count records for
-        alert_type (str):   Type of alert to get count for; None returns any/all alert types
+        resource_name (str):  Resource to get counts for
+        dev_id (str):         ID of device to get count for
+        reading_type (str):   Type of reading to get count for; None returns any/all reading types
 
     Returns:
         Status code from the HTTP request.
     """
     if verbose:
-        print('Counting collection {}, dev_id=\'{}\', alert_type=\'{}\''.format(collection, dev_id, alert_type), flush=True)
+        print('Counting {}, dev_id=\'{}\', reading_type=\'{}\''.format(resource_name, dev_id, reading_type), flush=True)
 
     assert dev_id is not None           # Should never happen ....
 
     params = {'dev-id': dev_id}
-    if alert_type is not None:
-        params['alert-type'] = alert_type
-    response = requests.get(CONFIG_DATA['get_count_url'][collection], params=params)
+    if reading_type is not None:
+        params['reading-type'] = reading_type
+    response = requests.get(CONFIG_DATA['get_count_url'][resource_name], params=params)
 
     if verbose:
         print('  count = {}'.format(int(response.content)), flush=True)
@@ -131,14 +133,14 @@ def test_1_message_in_range():
     Data file: test_1_message_in _range.csv
     """
     # Wipe the DB for our test device
-    delete_records(['readings', 'active_alerts', 'alert_history'], 'razpi_sim_01')
+    delete_resources(['readings', 'active_alerts', 'alert_history'], 'razpi_sim_01')
 
     # Send the readings
     status = send_sensor_reading_messages('test_1_message_in_range.csv')
     assert status == 200
 
     # Confirm the correct count of readings got stored in the DB
-    status, count = doc_count('readings', 'razpi_sim_01')
+    status, count = resource_count('readings', 'razpi_sim_01')
     assert status == 200
     assert count == 1
 
@@ -151,19 +153,19 @@ def test_10_messages_in_range():
     Data file: test_10_messages_in_range.csv
     """
     # Wipe the DB for our test device
-    delete_records(['readings', 'active_alerts', 'alert_history'], 'razpi_sim_01')
+    delete_resources(['readings', 'active_alerts', 'alert_history'], 'razpi_sim_01')
 
     # Send the readings
     status = send_sensor_reading_messages('test_10_messages_in_range.csv')
     assert status == 200
 
     # Confirm the correct count of readings got stored in the DB
-    status, count = doc_count('readings', 'razpi_sim_01')
+    status, count = resource_count('readings', 'razpi_sim_01')
     assert status == 200
     assert count == 10
 
     # Confirm no alert was generated
-    status, count = doc_count('active_alerts', 'razpi_sim_01')
+    status, count = resource_count('active_alerts', 'razpi_sim_01')
     assert status == 200
     assert count == 0
 
@@ -175,19 +177,19 @@ def test_trigger_temp_alert():
     Data file: test_trigger_temp_alert.csv
     """
     # Wipe the DB for our test device
-    delete_records(['readings', 'active_alerts', 'alert_history'], 'razpi_sim_01')
+    delete_resources(['readings', 'active_alerts', 'alert_history'], 'razpi_sim_01')
 
     # Send the readings
     status = send_sensor_reading_messages('test_trigger_temp_alert.csv')
     assert status == 200
 
     # Confirm the correct count of readings got stored in the DB
-    status, count = doc_count('readings', 'razpi_sim_01')
+    status, count = resource_count('readings', 'razpi_sim_01')
     assert status == 200
     assert count == 10
 
     # Confirm alert was generated for temperature
-    status, count = doc_count('active_alerts', 'razpi_sim_01', 'temp')
+    status, count = resource_count('active_alerts', 'razpi_sim_01', 'temp')
     assert status == 200
     assert count == 1
 
@@ -199,19 +201,19 @@ def test_trigger_and_clear_temp_alert():
     Data file: test_trigger_and_clear_temp_alert_pt1.csv & test_trigger_and_clear_temp_alert_pt2.csv
     """
     # Wipe the DB for our test device
-    delete_records(['readings', 'active_alerts', 'alert_history'], 'razpi_sim_01')
+    delete_resources(['readings', 'active_alerts', 'alert_history'], 'razpi_sim_01')
 
     # Send the first part of the readings; should trigger the alert
     status = send_sensor_reading_messages('test_trigger_and_clear_temp_alert_pt1.csv')
     assert status == 200
 
     # Confirm the correct count of readings got stored in the DB
-    status, count = doc_count('readings', 'razpi_sim_01')
+    status, count = resource_count('readings', 'razpi_sim_01')
     assert status == 200
     assert count == 5
 
     # Confirm alert was generated for temperature
-    status, count = doc_count('active_alerts', 'razpi_sim_01', 'temp')
+    status, count = resource_count('active_alerts', 'razpi_sim_01', 'temp')
     assert status == 200
     assert count == 1
 
@@ -220,17 +222,17 @@ def test_trigger_and_clear_temp_alert():
     assert status == 200
 
     # Confirm the correct count of readings got stored in the DB
-    status, count = doc_count('readings', 'razpi_sim_01')
+    status, count = resource_count('readings', 'razpi_sim_01')
     assert status == 200
     assert count == 10
 
     # Confirm alert was cleared
-    status, count = doc_count('active_alerts', 'razpi_sim_01', 'temp')
+    status, count = resource_count('active_alerts', 'razpi_sim_01', 'temp')
     assert status == 200
     assert count == 0
 
     # And we should now have a record in alert history
-    status, count = doc_count('alert_history', 'razpi_sim_01', 'temp')
+    status, count = resource_count('alert_history', 'razpi_sim_01', 'temp')
     assert status == 200
     assert count == 1
 
